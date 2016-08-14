8b00242770e4de6b668f6f7b103ac1b08443abf6
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-14 11:38:02 +0900

    remove users route. set up index route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+  var data = { time: req.params.time };
+  res.json(data);
 });
 
 module.exports = router;
